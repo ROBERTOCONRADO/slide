ab20b9c53b20a5719bb4b34eee5ff0939a048501
Roberto Conrado <robertoconradorc@gmail.com>
2022-09-24 14:04:38 -0300

    correcoes finais

--- js/slide.js
+++ js/slide.js
@@ -157,7 +157,6 @@ export default class SlideNav extends Slide {
         this.prevElement.addEventListener('click', this.activePrevSlide);
         this.nextElement.addEventListener('click', this.activeNextSlide);
     }
-
     createControl() {
         const control = document.createElement('ul');
         control.dataset.control = 'slide';
@@ -174,19 +173,16 @@ export default class SlideNav extends Slide {
         });
         this.wrapper.addEventListener('changeEvent', this.activeControlItem);
     }
-
     activeControlItem() {
         this.controlArray.forEach(item => item.classList.remove(this.activeClass));
         this.controlArray[this.index.active].classList.add(this.activeClass);
     }
-
     addControl(customControl) {
         this.control = document.querySelector(customControl) || this.createControl();
         this.controlArray = [...this.control.children];
         this.activeControlItem();
         this.controlArray.forEach(this.eventControl);
     }
-
     bindControlEvents() {
         this.eventControl = this.eventControl.bind(this);
         this.activeControlItem = this.activeControlItem.bind(this);
